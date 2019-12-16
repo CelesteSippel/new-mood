d061ff1de980e70adbd255c8f785f48c984e6441
Celeste Sippel <celestechristine@gmail.com>
2019-12-15 22:32:33 -0500

    done with homepage desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div></div>
+  return (
+    <div className="footer">
+      <div>
+        <h2 className="footer-text">
+          Sign Up for Future Shows and Announcements
+        </h2>
+      </div>
+      <div>
+        <form className="email-list">
+          <span>
+            <label className="padding">Email</label>
+          </span>
+          <input type="email" placeholder="Email Address" />
+          <span>
+            <input type="submit" value="Submit" className="button" />
+          </span>
+        </form>
+      </div>
+      <ul className="social-media">
+        <li>
+          <a href="https://www.instagram.com/newmoodband/">
+            <i className="fab fa-instagram space"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/thenewmood/">
+            <i className="fab fa-facebook-f space"></i>
+          </a>
+        </li>
+        <li>
+          <i className="fab fa-spotify space"></i>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UC6FuAGJqCidJf-PRrHY93rg">
+            <i class="fab fa-youtube space"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Footer
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import brewery from '../assets/brewery.JPG'
+import Footer from '../components/Footer'
 
 const HomePage = () => {
   return (
@@ -11,7 +12,17 @@ const HomePage = () => {
           <div className="hero-pic">
             <img src={brewery} />
           </div>
-          <div></div>
+          <div className="video">
+            <iframe
+              width="1417"
+              height="715"
+              src="https://www.youtube.com/embed/2k1jHbOtp9k"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <Footer />
         </main>
       </div>
     </>
