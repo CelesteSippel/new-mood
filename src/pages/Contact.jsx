d1fb72d8a5495eacdddc157d07aef6ecb4142502
Celeste Sippel <celestechristine@gmail.com>
2020-01-16 16:49:23 -0500

    added booking page and api call

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,26 +6,23 @@ import purple from '../assets/purple.JPG'
 import SideNav from '../components/SideNav'
 
 const Contact = () => {
-  const [name, setName] = useState('')
+  const [contactName, setContactName] = useState('')
   const [email, setEmail] = useState('')
   const [eventDate, setEventDate] = useState('')
   const [eventLocation, setEventLocation] = useState('')
   const [eventDetails, setEventDetails] = useState('')
 
-  // Need to create api post for this page
-  // ask how to make it so when they hit return in form it goes to the next input box
-
   const submitData = async e => {
     e.preventDefault()
     const resp = await axios.post('https://localhost:5001/api/Booking', {
-      name: name,
+      contactName: contactName,
       email: email,
       eventDate: eventDate,
       eventLocation: eventLocation,
       eventDetails: eventDetails,
     })
     console.log(resp.data)
-    setName('')
+    setContactName('')
     setEmail('')
     setEventDate('')
     setEventLocation('')
@@ -48,9 +45,9 @@ const Contact = () => {
                   placeholder="Name"
                   className="form-input"
                   onChange={e => {
-                    setName(e.target.value)
+                    setContactName(e.target.value)
                   }}
-                  value={name}
+                  value={contactName}
                   type="text"
                 />
               </section>
